8df4f33d0f00731d1e2a7382df2a34df6b835026
Matheus Reis <matheuss_reis@outlook.com>
2024-08-06 17:20:25 -0300

    Adição das importações

--- src/network/handler/request/request.handler.ts
+++ src/network/handler/request/request.handler.ts
@@ -1,3 +1,7 @@
-export class RequestHandler {
+import type { LoggerUtils } from "../../../utils/logger-utils";
+import type { RequestMessageInterface } from "./request-message.interface";
 
+export class RequestHandler {
+    private _messages: Map<number, RequestMessageInterface>;
+    private _logger: LoggerUtils;
 }
