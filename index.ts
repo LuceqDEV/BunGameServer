6ccc2958f458b0f52901c6bbd3c7d57a3d168f16
Matheus Reis <matheuss_reis@outlook.com>
2024-08-06 22:07:47 -0300

    Index finalizado

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { SetupServer } from "./src/server/setup.server";
+
+async function main() {
+    const server: SetupServer = new SetupServer();
+
+    await server.start();
+}
+
+main();
